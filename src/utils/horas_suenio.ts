f47c09e80ce7f74bc1dd42a27bfcec2b12ff8786
Maggy30ev <maggy30ev@gmail.com>
2023-04-23 20:13:36 -0600

    Función que calcula las 6 posibles horas para despertar

--- src/utils/horas_suenio.ts
+++ src/utils/horas_suenio.ts
@@ -33,3 +33,39 @@ export function horas_despertar(hora: number, minutos: number, am_pm: string): s
 
 	return horas;
 }
+
+// Funnción que retorna las 6 posibles horas de dormir, según la hora de despertarse
+// que se ingresó como parámetro
+
+export function horas_dormir(hora: number, minutos: number, am_pm: string): string[] {
+    let horas: string[] = [];
+
+    // Si es AM, se le suma 24 para convertirlo a 24 horas
+    hora = am_pm == "PM" ? hora+=12 : hora+=24
+
+    // Declaramo un objeto Date con la hora de despertarse
+    let tiempo = new Date();
+    tiempo.setHours(hora);
+    tiempo.setMinutes(minutos);
+
+    // Restamos 90 minutos a el objeto Date
+    tiempo.setMinutes(tiempo.getMinutes() - 90);
+
+    // Agregamos las 6 posibles horas para dormir al arreglo
+    for (let i = 0; i < 6; i++) {
+        let h_12 = tiempo.getHours() > 12 ? tiempo.getHours() - 12 : tiempo.getHours();
+        if (h_12 == 0) h_12 = 12;
+        let h = h_12 < 10 ? "0" + h_12 : h_12;
+        let m = tiempo.getMinutes() < 10 ? "0" + tiempo.getMinutes() : tiempo.getMinutes();
+
+        horas.push(h + ":" + m+ " " + (tiempo.getHours() >= 12 ? "PM" : "AM"));
+        
+        tiempo.setMinutes(tiempo.getMinutes() - 90);
+    }
+
+    // Invertimos el arreglo 
+
+    horas = horas.reverse();
+
+    return horas;
+}
